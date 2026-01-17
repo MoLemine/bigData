e668ff20e4102b55082e04de8c699100825ecd75
MoLemine <eminetah@gmail.com>
2026-01-17 19:30:19 +0000

    All in All

--- cmds/stop_mongodb.ps1
+++
@@ -1,4 +0,0 @@
-# stop_mongodb_cluster.ps1
-Write-Host "Arret du cluster MongoDB..." -ForegroundColor Red
-Get-Process mongod,mongos -ErrorAction SilentlyContinue | Stop-Process -Force
-Write-Host "Cluster arrete!" -ForegroundColor Green
